3c00655b7f4ed3ebd3d1552dd9a81df09d1687a6
Janka Uryga <lolzatu2@gmail.com>
2025-02-22 22:13:29 +0100

    convert tabs to spaces

--- fake_x1c/fake_x1c.py
+++ fake_x1c/fake_x1c.py
@@ -6,27 +6,24 @@ from time import sleep
 
 ANNOUNCEMENT_TEMPLATE = \
     "NOTIFY * HTTP/1.1\r\n" \
-	"Host: 239.255.255.250:1990\r\n" \
-	"Server: Buildroot/2018.02-rc3 UPnP/1.0 ssdpd/1.8\r\n" \
-	"Location: {printerip}\r\n" \
-	"NT: urn:bambulab-com:device:3dprinter:1\r\n" \
-	"NTS: ssdp:alive\r\n" \
-	"USN: 00M09C410701524\r\n" \
-	"Cache-Control: max-age=1800\r\n" \
-	"DevModel.bambu.com: BL-P001\r\n" \
-	"DevName.bambu.com: {printername}\r\n" \
-	"DevSignal.bambu.com: -58\r\n" \
-	"DevConnect.bambu.com: lan\r\n" \
-	"DevBind.bambu.com: free\r\n" \
-	"Devseclink.bambu.com: secure\r\n" \
-	"DevInf.bambu.com: wlan0\r\n" \
-	"\r\n"
+    "Host: 239.255.255.250:1990\r\n" \
+    "Server: Buildroot/2018.02-rc3 UPnP/1.0 ssdpd/1.8\r\n" \
+    "Location: {printerip}\r\n" \
+    "NT: urn:bambulab-com:device:3dprinter:1\r\n" \
+    "NTS: ssdp:alive\r\n" \
+    "USN: 00M09C410701524\r\n" \
+    "Cache-Control: max-age=1800\r\n" \
+    "DevModel.bambu.com: BL-P001\r\n" \
+    "DevName.bambu.com: {printername}\r\n" \
+    "DevSignal.bambu.com: -58\r\n" \
+    "DevConnect.bambu.com: lan\r\n" \
+    "DevBind.bambu.com: free\r\n" \
+    "Devseclink.bambu.com: secure\r\n" \
+    "DevInf.bambu.com: wlan0\r\n" \
+    "\r\n"
 
 UDP_PORT_NO = 2021
 
-def main(printerip, printername, interval=5.0):
-    interfaces = socket.getaddrinfo(host=socket.gethostname(), port=None, family=socket.AF_INET)
-    allips = [ip[-1][0] for ip in interfaces]
 
     msg = ANNOUNCEMENT_TEMPLATE.format(printerip='10.0.0.217', printername="UKS-UKS-UKS").encode()
     while True:
